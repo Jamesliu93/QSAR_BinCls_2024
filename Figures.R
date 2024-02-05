b79930520d23f7c2d3462c9c1ba78e63a57a0e1d
James Liu <james.yan.liu.93@gmail.com>
2024-02-05 16:28:55 -0600

    updated figs

--- Figures.R
+++ Figures.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(ggpubr)
+library(viridis)
 
 met <- read.csv("LSI_MULTI_MET.csv",header=TRUE,fileEncoding='UTF-8-BOM')
 met$Model <- as.factor(met$Model)
@@ -7,22 +7,33 @@ met$Model <- as.factor(met$Model)
 t1 <- theme_bw()+
   theme(text=element_text(size=10,color="black"),
         legend.text=element_text(size=10,color="black"),
-        axis.title.x=element_text(size=8,color="black"),
-        axis.title.y=element_text(size=8,color="black"),
-        axis.text.y=element_text(size=8,color="black"),
-        axis.text.x=element_text(size=8,color="black",angle=45,vjust=0.7)
+        axis.title.x=element_text(size=10,color="black"),
+        axis.title.y=element_text(size=10,color="black"),
+        axis.text.y=element_text(size=10,color="black"),
+        axis.text.x=element_text(size=10,color="black",angle=45,vjust=0.7)
         )
 
-p1 <- ggplot(met,aes(fill=Model))+t1+
+pl <- list(
+  t1,
+  geom_point(pch=21,size=3,stroke=0.7,color='black'),
+  scale_fill_viridis(discrete=T)
+)
+
+p1 <- ggplot(met,aes(fill=Model,x=Recall,y=Precision))+pl+
   scale_x_continuous(expand=c(0,0),limits=c(0,1))+
   scale_y_continuous(expand=c(0,0),limits=c(0.5,1))+
-  xlab('Recall')+ylab('Precision')+
-  geom_point(aes(x=Recall,y=Precision),size=5,color='black',pch=21)
-p1
+  xlab('Recall')+ylab('Precision')
+  
 
-p2 <- ggplot(met,aes(fill=Model))+t1+
+p2 <- ggplot(met,aes(fill=Model,x=Accuracy,y=F1_Score))+pl+
   scale_x_continuous(expand=c(0,0),limits=c(0.5,0.8))+
   scale_y_continuous(expand=c(0,0),limits=c(0,1))+
-  xlab('Accuracy')+ylab('F1 Score')+
-  geom_point(aes(x=Accuracy,y=F1_Score),size=5,color='black',pch=21)
+  xlab('Accuracy')+ylab('F1 Score')
+
+tiff("REC-PRE.tiff",height=2,width=3.25,units='in',res=300,compression="lzw")
+p1
+dev.off()
+
+tiff("ACC-F1S.tiff",height=2,width=3.25,units='in',res=300,compression="lzw")
 p2
+dev.off()
