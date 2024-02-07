c33a8af3126d028e5873ad662baf76f7aaf33a21
James Liu <james.yan.liu.93@gmail.com>
2024-02-07 14:14:14 -0600

    updated figs

--- LSI_FIG.R
+++ LSI_FIG.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(scales)
 library(viridis)
 
 met <- read.csv("LSI_MULTI_MET.csv",header=TRUE,fileEncoding='UTF-8-BOM')
@@ -20,20 +21,24 @@ pl <- list(
 )
 
 p1 <- ggplot(met,aes(fill=Model,x=Recall,y=Precision))+pl+
-  scale_x_continuous(expand=c(0,0),limits=c(0,1))+
-  scale_y_continuous(expand=c(0,0),limits=c(0.5,1))+
+  scale_x_continuous(expand=c(0,0),labels=scales::number_format(accuracy=0.1),
+                     limits=c(0.2,1),breaks=c(0.2,0.4,0.6,0.8,1))+
+  scale_y_continuous(expand=c(0,0),labels=scales::number_format(accuracy=0.1),
+                     limits=c(0.5,0.9),breaks=c(0.5,0.6,0.7,0.8,0.9))+
   xlab('Recall')+ylab('Precision')
   
 
 p2 <- ggplot(met,aes(fill=Model,x=Accuracy,y=F1_Score))+pl+
-  scale_x_continuous(expand=c(0,0),limits=c(0.5,0.8))+
-  scale_y_continuous(expand=c(0,0),limits=c(0,1))+
+  scale_x_continuous(expand=c(0,0),labels=scales::number_format(accuracy=0.1),
+                     limits=c(0.5,0.9),breaks=c(0.5,0.6,0.7,0.8,0.9))+
+  scale_y_continuous(expand=c(0,0),labels=scales::number_format(accuracy=0.1),
+                     limits=c(0.2,1),breaks=c(0.2,0.4,0.6,0.8,1))+
   xlab('Accuracy')+ylab('F1 Score')
 
-tiff("REC-PRE.tiff",height=2,width=3.25,units='in',res=300,compression="lzw")
+tiff("LSI_REC-PRE.tiff",height=2,width=3.25,units='in',res=300,compression="lzw")
 p1
 dev.off()
 
-tiff("ACC-F1S.tiff",height=2,width=3.25,units='in',res=300,compression="lzw")
+tiff("LSI_ACC-F1S.tiff",height=2,width=3.25,units='in',res=300,compression="lzw")
 p2
 dev.off()
