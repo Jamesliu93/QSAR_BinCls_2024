96d70f854b3ba73a221dcc3824673effa082b87a
James Liu <james.yan.liu.93@gmail.com>
2024-02-08 21:35:04 -0600

    updated RS

--- RS_FIG.R
+++ RS_FIG.R
@@ -8,6 +8,9 @@ met$Model <- as.factor(met$Model)
 prc <- read.csv("RS_MULTI_PRC.csv",header=TRUE,fileEncoding='UTF-8-BOM')
 prc$M <- as.factor(prc$M)
 
+roc <- read.csv("RS_MULTI_ROC.csv",header=TRUE,fileEncoding='UTF-8-BOM')
+roc$M <- as.factor(roc$M)
+
 t1 <- theme_bw()+
   theme(text=element_text(size=10,color="black"),
         legend.text=element_text(size=10,color="black"),
@@ -25,7 +28,7 @@ l1 <- list(
 
 p1 <- ggplot(met,aes(fill=Model,x=Recall,y=Precision))+l1+
   scale_x_continuous(expand=c(0,0),labels=scales::number_format(accuracy=0.1),
-                     limits=c(0.2,0.7),breaks=c(0.2,0.3,0.4,0.5,0.6,0.7))+
+                     limits=c(0.3,0.8),breaks=c(0.3,0.4,0.5,0.6,0.7,0.8))+
   scale_y_continuous(expand=c(0,0),labels=scales::number_format(accuracy=0.1),
                      limits=c(0.5,1),breaks=c(0.5,0.6,0.7,0.8,0.9,1))+
   xlab('Recall')+ylab('Precision')
@@ -53,9 +56,20 @@ l2 <- list(
   xlab('Recall'),
   ylab('Precision')
 )
+l3 <- list(
+  t1,
+  geom_path(aes(x=F,y=T,color=M),alpha=0.5),
+  scale_color_viridis(discrete=T),
+  xlab('False Positive Rate'),
+  ylab('True Positive Rate')
+)
 
 p3 <- ggplot(prc)+l2
+p4 <- ggplot(roc)+l3
 
-tiff("RS_PRC.tiff",height=6,width=6,units='in',res=300,compression="lzw")
+tiff("RS_PRC.tiff",height=4,width=6,units='in',res=300,compression="lzw")
 p3
+dev.off()
+tiff("RS_ROC.tiff",height=4,width=6,units='in',res=300,compression="lzw")
+p4
 dev.off()
